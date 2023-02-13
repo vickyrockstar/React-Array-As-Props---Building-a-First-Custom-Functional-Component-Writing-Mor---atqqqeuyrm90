823dcf855233ecd2afb273e759a368a5b9be3a72
vighnesh shetty <vighneshshetty79@gmail.com>
2023-02-13 23:48:53 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import '../styles/App.css';
-const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
+import React from "react";
+import "../styles/App.css";
+const arr = JSON.parse(
+  window.localStorage.getItem("props") || `["hello","world"]`
+); // do not change
 
-const Join = (props) =>{
-  return(
-    <div id ="join">
-      {props.words.join(", ")}
+const Join = (props) => {
+  return (
+    <div id="join">
+      {props.words.join(",")}
       {/* Access prop 'words' and print it using .join like words.join(',')*/}
     </div>
-  )
-}
+  );
+};
+
 const App = () => {
   return (
-    <div id="root">
-      <Join  words={arr} />
+    <div id="main">
+      <Join words={arr} />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
